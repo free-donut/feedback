b6ced7402ba74e6f5760bc4d16bc3f7358710952
free-donut <yula@freedonut.space>
2022-01-29 22:44:27 +0300

    Home page

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Appeal;
+use App\Form\AppealType;
+use App\Repository\AppealRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function home(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        // just set up a fresh $task object (remove the example data)
+        $appeal = new Appeal();
+        $form = $this->createForm(AppealType::class, $appeal, ['showStatus' => false]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($appeal);
+            $entityManager->flush();
+
+
+            // ... perform some action, such as saving the task to the database
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->renderForm('pages/home.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}
